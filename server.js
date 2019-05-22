fc8f9eaaa817a4182941e172da0865798c6820ac
Toni Margan <toni.margan@gmail.com>
2019-05-22 14:43:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const next = require('next')
 
+const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
@@ -20,7 +21,7 @@ app
       return handle(req, res)
     })
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err
       console.log('> Ready on http://localhost:3000')
     })
