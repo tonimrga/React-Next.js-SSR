4d289b68351f9831e9bf917f65ef83d207b3c308
Toni Margan <toni.margan@gmail.com>
2019-05-31 16:14:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app
 
     server.get('/post/:title', (req, res) => {
       const actualPage = '/post'
-      const queryParams = { title: process.env.key }
+      const queryParams = { title: req.params.title }
       app.render(req, res, actualPage, queryParams)
     })
 
